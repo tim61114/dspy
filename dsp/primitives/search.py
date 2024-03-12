f087a94558bbf8cb96698675baaf20912cc83d6e
Tim Fan <tfan@snaplogic.com>
2024-03-12 13:51:38 -0700

    Modified dsp/primitives/search.py behavior to enable pinecone usage. Added test_rag notebook for testing capabilities and general usages.

--- dsp/primitives/search.py
+++ dsp/primitives/search.py
@@ -9,7 +9,7 @@ def retrieve(query: str, k: int, **kwargs) -> list[str]:
     """Retrieves passages from the RM for the query and returns the top k passages."""
     if not dsp.settings.rm:
         raise AssertionError("No RM is loaded.")
-    passages = dsp.settings.rm(query, k=k, **kwargs)
+    passages = dsp.settings.rm(query, **kwargs).passages
     if not isinstance(passages, Iterable):
         # it's not an iterable yet; make it one.
         # TODO: we should unify the type signatures of dspy.Retriever
